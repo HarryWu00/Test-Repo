6f637625073aa1ddce7dcecfc0b4116f8362afec
HarryWu00 <8hwu@stpatrick.org>
2016-10-07 10:23:38 -0500

    fix the error

--- octocats.js
+++ octocats.js
@@ -1,28 +1,22 @@
 //Write your constructor function below.
 //You should have 2 instance variables and 1 instance function included.
-function Octocat(name,arms){
+function Octocat(n,a){
 
   this.name = n
   this.arms = a
+  this.slap = function(){
 
-  this.slap = fucntion(){
-    For(var a=0, a<arms, a++){
-      conosle.log("SLAP")
+    for(var i=0;i<this.arms;i++){
+      conosle.log("SLAP");
     }
   }
 }
 
 
 
-
-
-
-
-
-
-
 //Write your 3 new octocat objects below here.
-var myoctocat = new Octocat( "Aden",8)
-var hisoctocat = new Octocat("joe",9)
+var myoctocat = new Octocat("Aden",563)
+var hisoctocat = new Octocat("joe",1616)
+var heroctocat = new Octocat("123",6565)
 
-console.log(myoctocat,"name")
+console.log(myoctocat,"arms")
